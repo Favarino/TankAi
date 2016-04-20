64f2fb36588a267a55f6170a26937628ebfedde3
Nicole Long <n.long1520@gmail.com>
2016-04-20 16:13:16 -0700

    Ai

---
+++ TankClientLib/TankBattle-client-dyad-sfw/Agent.h
@@ -0,0 +1,98 @@
+#pragma once
+#include "TankBattleHeaders.h"
+#include "Vector2.h"
+#include <math.h>
+#include <iostream>
+
+
+class Agent
+{
+	tankNet::TankBattleStateData *current;
+	tankNet::TankBattleCommand tbc;
+	
+	enum Turret {SCAN,AIM,FIRE} turret = SCAN;
+	enum Tank {EVADE, CHASE, RANDOMIZE, RETREAT } tank = CHASE;
+	size_t aimTarget;
+	void Scan()
+	{
+		tbc.cannonMove = tankNet::CannonMovementOptions::RIGHT;
+		tbc.fireWish = 0;
+		for (aimTarget = 0; aimTarget < 3; ++aimTarget)
+		{
+			if (current->tacticoolData[aimTarget].inSight)
+			{
+				turret = AIM;
+				tbc.cannonMove = tankNet::CannonMovementOptions::HALT;
+			}
+		}
+	}	
+	void Aim()
+	{
+
+		Vector2 dir = normal(Vector2(current->position[0], current->position[2]) - Vector2(current->tacticoolData->lastKnownPosition[0], current->tacticoolData->lastKnownPosition[2]));
+
+		for (aimTarget = 0; aimTarget < 3; ++aimTarget)
+		{
+			if (current->tacticoolData[aimTarget].inSight)
+			{
+				float boop = dot(perp(dir), Vector2(current->cannonForward[0], current->cannonForward[2]));
+
+				//std::cout << boop << "\n";
+
+				if (dot(perp(dir), Vector2(current->cannonForward[0], current->cannonForward[2])) > 0)
+				{
+					tbc.cannonMove = tankNet::CannonMovementOptions::LEFT;
+				}
+				if (dot(perp(dir), Vector2(current->cannonForward[0], current->cannonForward[2])) < 0)
+				{
+					tbc.cannonMove = tankNet::CannonMovementOptions::RIGHT;
+				}
+
+				if (dot(perp(dir), Vector2(current->cannonForward[0], current->cannonForward[2])) > .01)
+				{
+					turret = FIRE;
+				}
+			}
+		}
+	}
+	void Fire()
+	{
+		tbc.cannonMove = tankNet::CannonMovementOptions::HALT;
+		tbc.fireWish = 1;
+		turret = SCAN;
+	}
+	
+	void Chase()
+	{
+		float dis = distance(Vector2(current->position[0], current->position[2]), Vector2(current->tacticoolData->lastKnownPosition[0], current->tacticoolData->lastKnownPosition[2]));
+		std::cout << dis << "\n";
+		if (dis > 13)
+		{
+			tbc.tankMove = tankNet::TankMovementOptions::FWRD;
+		}
+		else
+		{
+			tbc.tankMove = tankNet::TankMovementOptions::HALT;
+		}
+	}
+public:
+	tankNet::TankBattleCommand update(tankNet::TankBattleStateData *state)
+	{
+		
+		current = state;
+
+		switch (turret)
+		{
+			case SCAN : Scan(); break;
+			case AIM: Aim(); break;
+			case FIRE: Fire(); break;
+		}
+		switch (tank)
+		{
+		case CHASE: Chase(); break;
+		}
+		return tbc;
+		
+	    
+	}
+};
